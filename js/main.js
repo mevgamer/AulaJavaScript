12a35ef08050d4541a7fc80c4d1f5d7ae81e88a9
Thiago Torres <terrormaluco@gmail.com>
2021-01-18 18:52:37 -0300

    aula functions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+function soma(n1,n2){
+    return n1+n2;
+}
+
+function validaIdade(idade){
+    var valida;
+    if (idade>=18) {
+        validar = true;
+    }elsee{
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("qual a sua idade?");
+console.log(validaIdade(idade));
+
+//alert(soma(5,10))
+
 
 /*
 var d = new Date();
