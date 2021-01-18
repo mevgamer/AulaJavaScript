db92f0a2e6473f2da991bcf858c00aa17975d733
Thiago Torres <terrormaluco@gmail.com>
2021-01-18 15:20:03 -0300

    primeira aula javascript

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var nome = "Thiago Torres";
+var n1 = 21;
+var n2 = 25;
+var frase = "Japão é o melhor time do mundo!"
+
+//alert(nome + "tem" + idade + "anos");
+
+//alert(idade + idade2)
+
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.replace("Japão","Brasil"));
+alert(frase.replace("Japão","Brasil"));
