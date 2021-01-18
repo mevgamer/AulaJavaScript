57a20ce1520199a913c4668714b5d7d40defa9d0
Thiago Torres <terrormaluco@gmail.com>
2021-01-18 15:34:23 -0300

    Aula 2, array

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
-var nome = "Thiago Torres";
-var n1 = 21;
-var n2 = 25;
-var frase = "Japão é o melhor time do mundo!"
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva" , cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maçã","pera",'laranja'];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista.length);
+//console.log(lista.toString());
+//console.log(lista.join(" - "))
+
+
+
+
+//var nome = "Thiago Torres";
+//var n1 = 21;
+//var n2 = 25;
+//var frase = "Japão é o melhor time do mundo!"
 
 //alert(nome + "tem" + idade + "anos");
 
 //alert(idade + idade2)
 
-console.log(nome);
-console.log(n1 * n2);
-console.log(frase.replace("Japão","Brasil"));
-alert(frase.replace("Japão","Brasil"));
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.replace("Japão","Brasil"));
+//alert(frase.replace("Japão","Brasil"));
